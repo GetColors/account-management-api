2936adc2d1cd686196ceb265a2fc05bc52ee13ca
camilo alaniz <calaniz@option.cl>
2019-01-19 19:51:28 -0300

    fixed database name

--- database/atenas.sql
+++ database/atenas.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `proyecto_ciisa` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-USE `proyecto_ciisa`;
+CREATE DATABASE  IF NOT EXISTS `atenas` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
+USE `atenas`;
 -- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: proyecto_ciisa
